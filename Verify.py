5132925f84e228404ff26add603643f3b54c817c
TronVerma <tron.verma@gmail.com>
2016-10-29 23:42:34 +0530

    Removed Subscription key and Face ID

--- Verify.py
+++ Verify.py
@@ -6,14 +6,14 @@ import httplib, urllib, base64
 headers = {
     # Request headers
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '4e3ecd34501e4b52ad056ed7df847705',
+    'Ocp-Apim-Subscription-Key': '',
 }
 
 params = urllib.urlencode({
 })
 try:
     conn = httplib.HTTPSConnection('api.projectoxford.ai')
-    conn.request("POST", "/face/v1.0/identify?%s" % params, "{'personGroupId':'001','faceIds':['fe2aac59-7bca-4dcb-b09d-24d4bd3db37f'],'maxNumOfCandidatesReturned' : 1 }", headers)
+    conn.request("POST", "/face/v1.0/identify?%s" % params, "{'personGroupId':'001','faceIds':[''],'maxNumOfCandidatesReturned' : 1 }", headers)
     response = conn.getresponse()
     data = response.read()
     print(data+"\n")
